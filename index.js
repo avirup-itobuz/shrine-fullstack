6cbb77801336db7ae008d9116e969f0815b8c48c
avirup-itobuz <avirup@itobuz.com>
2024-03-11 18:40:56 +0530

    nav and hero done

---
+++ index.js
@@ -0,0 +1,18 @@
+// //navbar
+// const navbar = document.getElementById("nav");
+// const navCode = document.getElementById("navbarCode");
+// navbar.innerHTML = navCode;
+
+// //hero
+// const energiseYourLife = document.getElementById("energise-your-life");
+// const faithInAction = document.getElementById("faith-in-action");
+
+// energiseYourLife.children[0].children[0].src =
+//   "assets/home/hero/energiseYourLife.png";
+// energiseYourLife.children[1].innerText = "Energise your life";
+// energiseYourLife.children[2].innerText = "Long established fact that";
+
+// faithInAction.children[0].children[0].src =
+//   "assets/home/hero/faithInAction.png";
+// faithInAction.children[1].children[0].innerText =
+//   "Faith in Action, Love in Motion";
